0b00d48d2355a59eec3b1ae1ff4a50c68cf12b15
revalinoaldi <revalinoaldi@gmail.com>
2021-12-21 16:54:34 +0700

    second Commit

--- application/views/incl/v_footer.php
+++ application/views/incl/v_footer.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-                    © 2019 Zinzer <span class="d-none d-md-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign.</span>
+                    © <?= date('Y') ?> PT. KUDHI PIRANTI <span class="d-none d-md-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Codeigniter 3.11.</span>
                 </footer>
